b3ef123da76f7e338f0d59f93064a0d5f9d1ec10
choiboi <james.choiboi@gmail.com>
2014-03-19 18:47:41 -0400

    New utils class for generating random numbers.

---
+++ Android/src/com/alottapps/randomizer/util/RandomGenerator.java
@@ -0,0 +1,32 @@
+package com.alottapps.randomizer.util;
+
+import java.util.ArrayList;
+
+
+public class RandomGenerator {
+
+    public static int singleRandomNumber(int bound) {
+        return (int) Math.random() * bound;
+    }
+    
+    public static int singleRangeRandomNumber(int start, int end) {
+        return start + (int)(Math.random() * ((end - start) + 1));
+    }
+    
+    public static ArrayList<Integer> listUniqueRandomNumber(int listSize, int bound) {
+        ArrayList<Integer> numL = new ArrayList<Integer>();
+        
+        int i = 0;
+        int num = -1;
+        while (i < listSize) {
+            num = singleRandomNumber(bound);
+            
+            if (!numL.contains(num)) {
+                numL.add(num);
+                i++;
+            }
+        }
+        
+        return numL;
+    }
+}
