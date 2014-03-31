f6b33650e80dd06c4e85616b90de408c5785628b
choiboi <james.choiboi@gmail.com>
2014-03-31 12:02:33 -0400

    Initial display saved list page.

---
+++ Android/src/com/alottapps/randomizer/SavedListsActivity.java
@@ -0,0 +1,13 @@
+package com.alottapps.randomizer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SavedListsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saved_lists);
+    }
+}
